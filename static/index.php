61ee3e44bc163ad4c36d13893d93e2e4422c5d84
Yovany Cancio <yovany.canciopaz@gmail.com>
2019-10-14 23:38:40 -0400

    removed slider

--- static/index.php
+++ static/index.php
@@ -1,27 +1,8 @@
 <?php include 'header.php'; ?>
    
  
-  <div id="slider" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#slider" data-slide-to="0" class="active"></li>
-    <li data-target="#slider" data-slide-to="1"></li> 
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active"> 
-    <img src="assets/img/slide1.jpg" alt="Dr Planter">
-    </div>
-    <div class="carousel-item"> 
-     <img src="assets/img/slide1.jpg" alt="Dr Planter">
-    </div> 
-  </div>
-  <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  <div class="hero-img">
+    <img src="assets/img/slide1.jpg" alt="Dr Planter" class="img-fluid">
 </div>
 
   <section id="content-main">
