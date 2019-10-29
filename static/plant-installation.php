2261f6001fd57dbbaad6e707093d5e42f588cba5
Yovany Cancio <yovany.canciopaz@gmail.com>
2019-10-28 23:51:49 -0400

    installations page

--- static/plant-installation.php
+++ static/plant-installation.php
@@ -1,11 +1,24 @@
 <?php include 'header.php'; ?>
- 
-<h1 class="display-4">Plant Installations</h1>
+<img src="assets/img/plant_installations.png" alt="Plant Installations" class="img-fluid"> 
+
 
 <div class="row">
-<div class="col-sm-7">
-		<p class="lead">We know how important it is to create a lasting impression for any customer, client, and most importantly, employees - and plants are the best way to do just that. Spicing up interior spaces by creating grand entryways, adding life to open spaces, and freshening up creative work spaces are just a few ideas to accomplish that. And from creating a cohesive landscape of a new mall, to adding a fresh plants to a retail space to show a unique style, or even floral bud vases to add color to tables, we can bring any retail space to life!</p>
-		<br>
+<div class="col-lg-7">
+	<h1 class="display-4">Plant Installations</h1>
+	<p class="lead">We know how important it is to create a lasting impression for any customer, client, and most importantly, employees - and plants are the best way to do just that. Spicing up interior spaces by creating grand entryways, adding life to open spaces, and freshening up creative work spaces are just a few ideas to accomplish that. And from creating a cohesive landscape of a new mall, to adding a fresh plants to a retail space to show a unique style, or even floral bud vases to add color to tables, we can bring any retail space to life!</p> 
+</div>
+	
+<div class="col-lg-5">
+	<br>
+	<img src="assets/img/plant_installations_right.png" class="img-fluid" alt="">
+</div> 
+
+<div class="col-lg-5">
+	<br>
+	<img src="assets/img/plant_installations_left.png" class="img-fluid" alt="">
+</div>
+
+<div class="col-lg-7"> 
 		<ul class="lead installations-list">
 			<li>Live Plant Installs </li>
 			<li>Artificial Plant Installs </li>
@@ -27,44 +40,50 @@
 			<li>Hospitality </li>
 			<li>Healthcare Facilities</li>
 		</ul>
-	</div>
-	
-	<div class="col-sm-5">
-		<img src="assets/img/p2.jpg" class="img-fluid" alt="">
-	</div> 
-	
-	<div class="col-sm-6 offset-sm-3">
-		<br><br>
-		<div class="contact-form">
+</div>
+
+</div>
+<br><br>
+<div class="contact-form">
 			<form>
 			  <h5><i class="fa fa-envelope" aria-hidden="true"></i> Connect with us to get started:</h5>
-			   <div class="form-group">
-			    <label for="name">Name <small class="text-danger">*</small></label>
-			    <input type="text" required class="form-control" id="name" placeholder="Enter your Name">
-			  </div> 
-			  <div class="form-group">
-			    <label for="company">Company</label>
-			    <input type="text" required class="form-control" id="company" placeholder="Enter your Company">
+			  <div class="row">
+			  	<div class="col-sm-6">
+				  <div class="form-group">
+					<label for="name">Name <small class="text-danger">*</small></label>
+					<input type="text" required class="form-control" id="name" placeholder="Enter your Name">
+				</div> 
 			  </div>
-			  <div class="form-group">
+			  	<div class="col-sm-6">
+				  <div class="form-group">
+					<label for="company">Company</label>
+					<input type="text" required class="form-control" id="company" placeholder="Enter your Company">
+				  </div>
+				</div>
+
+				<div class="col-sm-6"><div class="form-group">
 			    <label for="exampleInputEmail1">Email <small class="text-danger">*</small></label>
 			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-			  </div>
-			  <div class="form-group">
+			  </div></div>
+
+				<div class="col-sm-6"> <div class="form-group">
 			    <label for="phonenumber">Phone number</label>
 			    <input type="text" required class="form-control" id="phonenumber" placeholder="Enter your Phone number">
-			  </div>
+			  </div></div>
+
+			  <div class="col-sm-12">
 			  <div class="form-group">
 			    <label for="message">Tell us about your space</label>
 			    <textarea class="form-control" id="message" rows="3"></textarea>
 			  </div>
+			  </div>
+
+			  </div> 
+			 
 			 <!-- upload pictures TODO -->
 			  <button type="submit" class="btn btn-primary">Submit</button>
 			</form>
 		</div>
-	</div>
-
-</div>
 
 <?php include 'footer.php'; ?>
